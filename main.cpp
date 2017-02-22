68f7fd7abf8453d391f4dfa11db3515a3cf0e0e3
Стерликова Виктория Александровна <ip614s18@mwp08.csc.local>
2017-02-22 14:26:02 +0700

    step 1

--- main.cpp
+++ main.cpp
@@ -3,7 +3,19 @@
 #include <stdio.h>
 using namespace std;
   int main(){
-  
+  int s=0,d=0;
+ cout<<"enter s"<<endl;
+ cin>>s;
+ cout<<"enter d"<<endl;
+ cin>>d;
+  if(s<0){
+  cout<<"error"<<endl;
+  return 0;
+  }
+  if(d<0 && d>365){
+  cout<<"error"<<endl;
+  return 0;
+  }
   
   
   
